507fa32e919e234d1ef8102bf03b69fd1ba6ad0a
Jiri Bruchanov <jbruchanov@gmail.com>
2017-03-06 09:11:49 +0000

    Remove deleted route

--- routes/_init.js
+++ routes/_init.js
@@ -7,5 +7,4 @@ module.exports = (app) => {
     app.use('/', require('./index'));
     app.use('/users', require('./users'));
     app.use('/chat', require('./chat'));
-    app.use('/form', require('./form'));
 };
